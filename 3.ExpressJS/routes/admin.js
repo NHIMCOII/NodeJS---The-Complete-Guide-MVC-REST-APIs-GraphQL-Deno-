895a88ed19aa511f8483e19d1ec87ae27762aa95
NHIMCOII <90151567+NHIMCOII@users.noreply.github.com>
2022-05-29 21:53:48 +0700

    serving html

--- 3.ExpressJS/routes/admin.js
+++ 3.ExpressJS/routes/admin.js
@@ -1,11 +1,13 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path.js');
 
 const router = express.Router();
 // /admin/router => METHOD
 router.get('/add-product',(req, res, next) => {
-    res.send(
-        '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form'
-    );
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 router.post('/add-product', (req,res,next) =>{
