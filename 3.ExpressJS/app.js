d69a202b9712cf62c11bda3effdf8726226b69c3
NHIMCOII <90151567+NHIMCOII@users.noreply.github.com>
2022-05-30 17:07:34 +0700

    finishing the Pug Template

--- 3.ExpressJS/app.js
+++ 3.ExpressJS/app.js
@@ -20,7 +20,7 @@ app.use(shopRoutes);
 
 app.use((req,res,next) => {
     // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-    res.render('404.pug');
+    res.render('404.pug',{pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
